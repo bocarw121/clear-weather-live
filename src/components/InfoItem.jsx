8a04bd5d5e372d1128f69118f9196863e21a7de8
bocarw121 <bocaralhassanwane@gmail.com>
2021-07-15 19:37:52 -0400

    Adjust InfoItem and add uviLevelColor

--- src/components/InfoItem.jsx
+++ src/components/InfoItem.jsx
@@ -3,20 +3,45 @@ import { StyleSheet, Text, View } from "react-native";
 
 import colors from "../constants/colors";
 
-const InfoItem = ({ description, measure, unit }) => {
+const InfoItem = ({ info }) => {
+  const { name, value, level, unit } = info;
+  const uviStyle = [styles.uvi];
+
+  uviLevelColor(name, value, uviStyle);
   return (
     <View style={styles.container}>
-      <Text style={styles.description}>{description}</Text>
-      <Text style={styles.unit}>{`${measure} ${unit || ""}`}</Text>
+      <Text style={styles.description}>{name}</Text>
+      <Text style={styles.unit}>{`${value} ${unit || ""}`}</Text>
+      {name === "UVI" ? <Text style={uviStyle}>{level}</Text> : null}
     </View>
   );
 };
 
 export default InfoItem;
 
+const uviLevelColor = (name, value, uviStyle) => {
+  name === "UVI" && value >= 0 && value <= 2
+    ? uviStyle.push({ color: colors.uviLevels.low })
+    : null;
+  name === "UVI" && value >= 3 && value <= 5
+    ? uviStyle.push({ color: colors.uviLevels.moderate })
+    : null;
+  name === "UVI" && value >= 6 && value <= 7
+    ? uviStyle.push({ color: colors.uviLevels.high })
+    : null;
+  name === "UVI" && value >= 8 && value <= 10
+    ? uviStyle.push({ color: colors.uviLevels.veryHigh })
+    : null;
+  name === "UVI" && value >= 8 && value <= 10
+    ? uviStyle.push({ color: colors.uviLevels.extreme })
+    : null;
+};
+
 const styles = StyleSheet.create({
   container: {
     alignItems: "center",
+    justifyContent: "space-around",
+    fontWeight: "bold",
   },
   description: {
     color: colors.text.light,
@@ -25,7 +50,13 @@ const styles = StyleSheet.create({
   },
   unit: {
     color: colors.text.yellow,
-    fontSize: 18,
+    fontSize: 19,
     paddingTop: 10,
+  },
+  uvi: {
+    paddingTop: 5,
+    paddingBottom: 5,
+    fontSize: 18,
+    fontWeight: "bold",
   },
 });
--- src/components/__tests__/InfoItem.test.js
+++ src/components/__tests__/InfoItem.test.js
@@ -5,7 +5,13 @@ import { render } from "@testing-library/react-native";
 import InfoItem from "../InfoItem";
 
 it("Should render the InfoItem", () => {
-  const screen = render(<InfoItem />);
+  const item = {
+    name: "",
+    value: 0,
+    level: "low",
+    unit: "",
+  };
+  const screen = render(<InfoItem info={item} />);
 
   expect(screen).toMatchSnapshot();
 });
